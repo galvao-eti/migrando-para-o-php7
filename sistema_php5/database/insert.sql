fa4137cbfa526fca83babf17dac7750b9d909357
Er Galvão Abbott <galvao@galvao.eti.br>
2017-11-12 07:20:49 -0200

    Corrigindo inserção do usuário de testes (usuario@teste.com.br / usuario)

--- sistema_php5/database/insert.sql
+++ sistema_php5/database/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO usuario (email, senha) VALUES ('6EtOAN5y/vGszfLY+RMjJfnZEeUwokbyGT6H4ZA5YtM=', '$2y$10$SjoryI9KtTl4adbs19Sxhukl8br.f6Y.Wn/0ug7ULKnyMLJLqLgOu');
+INSERT INTO usuario (name, email, senha) VALUES ('Usuário Teste', '36de1f44446de7d3f2de89fae6779075b2a4f6e4', '$2y$10$tvW0UMaTQjVPAuIrTkte4.snJrrYIph669Y2scj4YRYh9kXvKj2cm');
